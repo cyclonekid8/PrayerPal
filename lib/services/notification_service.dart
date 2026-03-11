7bb1ad75a145aee6fe63e9e70533efef7ef69ff2
cyclonekid8 <muzafarshahmaricab@gmail.com>
2026-03-12 01:54:14 +0800

    Fix missing dart:typed_data import

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,5 +1,6 @@
 // lib/services/notification_service.dart
 import 'dart:math';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
